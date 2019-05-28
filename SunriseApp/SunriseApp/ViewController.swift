bba8feefe176387b28c614d53c2793d96256f877
luecy1 <kowaheno@gmail.com>
2019-05-28 21:38:17 +0900

    SunriseApp作成状態をコミット

---
+++ SunriseApp/SunriseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SunriseApp
+//
+//  Created by 大沼　陽平 on 2019/05/28.
+//  Copyright © 2019 Youhei oonuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
